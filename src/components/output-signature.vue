25ce1d9a2fac10cda5bfea6a0a0d9973aadfc0c5
chrisroselli <chrisroselli@gmail.com>
2020-05-06 12:29:35 -0400

    add youtube link

--- src/components/output-signature.vue
+++ src/components/output-signature.vue
@@ -81,13 +81,13 @@
                     src=
                     "https://dc69b531ebf7a086ce97-290115cc0d6de62a29c33db202ae565c.ssl.cf1.rackcdn.com/1874/linkedin.png" /></a> -->
                     
-                    <!-- <a href="https://www.youtube.com/channel/UCop6hiVtGvTDem0UqPppINw"
+                    <a href="https://www.youtube.com/user/CTBasementSystems"
                     style=
                     "border-width:0px; border:0px; text-decoration: none;"><img width="25"
                     height="25" style=
                     "border: none; width: 25px; max-width: 25px !important; height: 25px; max-height: 25px !important;"
                     src=
-                    "https://dc69b531ebf7a086ce97-290115cc0d6de62a29c33db202ae565c.ssl.cf1.rackcdn.com/1874/youtube.png" /></a> -->
+                    "https://dc69b531ebf7a086ce97-290115cc0d6de62a29c33db202ae565c.ssl.cf1.rackcdn.com/1874/youtube.png" /></a>
                     <a href="https://twitter.com/CTBasementSys" target="_blank"
                     style=
                     "border-width:0px; border:0px; text-decoration: none;"><img width="25"
